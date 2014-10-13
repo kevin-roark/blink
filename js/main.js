8cbe1e2e1e7563eb901f5fb031da131a6f808ece
Kevin Roark <ker2143@columbia.edu>
2014-10-13 01:18:28 -0400

    initial garbage

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(function() {
+
+  var gus = document.querySelector('#gus');
+  var $gus = $(gus);
+
+  var vids = [gus];
+  var currentVideoIndex = 0;
+
+  var numMedia = vids.length;
+  var mediasReady = 0;
+
+  for (var i = 0; i < vids.length; i++) {
+    vids[i].addEventListener('canplaythrough', mediaReady);
+  }
+
+  function mediaReady() {
+    mediasReady++;
+    if (mediasReady == numMedia) {
+      start();
+    }
+  }
+
+  function start(restarting) {
+    $(vids[0]).show();
+
+    for (var i = 0; i < vids.length; i++) {
+      var vid = vids[i];
+      vid.play();
+    }
+  }
+
+  $('body').keypress(function(event) {
+    if (event.which == 32) { // spacebar
+      cycleToNextVideo();
+    }
+  });
+
+  function cycleToNextVideo() {
+    vids[currentVideoIndex].hide();
+
+    currentVideoIndex += 1;
+    if (currentVideoIndex >= vids.length) {
+      currentVideoIndex = 0;
+    }
+
+    vids[currentVideoIndex].show();
+  }
+
+});
